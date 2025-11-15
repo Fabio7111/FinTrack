f7e05a5dc0e7bce6fe01a536a68475033f125d87
Natã do Carmo Souza <nata.carmo05@gmail.com>
2025-11-15 09:53:12 -0300

    chore: create folder structure and configure DbContext
    Criada a estrutura de diretórios (Models, Controllers, Views, Data, Services e wwwroot).
    Adicionado FinTrackContext baseado em IdentityDbContext.
    Configurada connection string e registro do DbContext no Program.cs.

---
+++ Data/FinTrackContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinTrack.Data
+{
+    public class FinTrackContext : IdentityDbContext
+    {
+        public FinTrackContext(DbContextOptions<FinTrackContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets serão adicionados depois (Contas, Categorias, Transacoes, etc.)
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using FinTrack.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FinTrackContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
