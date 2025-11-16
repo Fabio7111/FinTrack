67902f0474accf73ce634319bbeffac74567830c
Natã do Carmo Souza <nata.carmo05@gmail.com>
2025-11-15 21:11:08 -0300

    feat(models): adicionar entidade TipoPagamento
    Criada a classe TipoPagamento com propriedades Id e Nome.
    
    Adicionados atributos de validação (Required, StringLength).

---
+++ Models/TipoPagamento.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinTrack.Models
+{
+    public class TipoPagamento
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do tipo de pagamento é obrigatório.")]
+        [StringLength(50)]
+        public string Nome { get; set; } = string.Empty;
+    }
+}
